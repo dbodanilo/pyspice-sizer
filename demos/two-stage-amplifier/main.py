de2245dbff2206155be78cc7dbdb1874fc909534
Danilo Bizarria <unifdaoliveira@fei.edu.br>
2024-04-26 11:47:57 -0300

    format two-stage-amplifier's main.py script.

--- demos/two-stage-amplifier/main.py
+++ demos/two-stage-amplifier/main.py
@@ -1,16 +1,20 @@
-import sys
-sys.path.append(".")
+import matplotlib.pyplot as plt
+import numpy as np
+
 import logging
+import sizer
+import sys
+
+
+sys.path.append(".")
 logger = logging.getLogger()
 # logger.setLevel(0)
 
-import sizer
-
-import numpy as np
-
+_PLOT = False
 
 with open("./demos/two-stage-amplifier/two-stage-amp.cir") as f:
     circuitTemplate = sizer.CircuitTemplate(f.read())
+
 
 def bandwidthLoss(circuit):
     try:
@@ -21,10 +25,12 @@ def bandwidthLoss(circuit):
         print("bandwidth undefined")
         return 1
 
+
 def gainLoss(circuit):
     return np.maximum(0, (1e+3 - np.abs(circuit.gain)) / 1e+3) ** 2
     # return np.maximum(0, (1e+3 - np.abs(circuit.gain)) / 1e+3)
     # return (1e+3 - np.abs(circuit.gain)) / 1e+3
+
 
 def phaseMarginLoss(circuit):
     try:
@@ -33,8 +39,10 @@ def phaseMarginLoss(circuit):
     except:
         return 0
 
+
 def loss(circuit):
     return np.sum([phaseMarginLoss(circuit), gainLoss(circuit), bandwidthLoss(circuit)])
+
 
 bounds = {
     w: [3.5e-7, 3.5e-4] for w in ["w12", "w34", "w5", "w6", "w7", "w8"]
@@ -48,7 +56,9 @@ bounds.update({
     "cm": [1e-12, 10e-12]
 })
 
-optimizer = sizer.optimizers.Optimizer(circuitTemplate, loss, bounds, earlyStopLoss=0)
+optimizer = sizer.optimizers.Optimizer(
+    circuitTemplate, loss, bounds, earlyStopLoss=0
+)
 # optimizer = sizer.optimizers.ScipyMinimizeOptimizer(circuitTemplate, loss, bounds, earlyStopLoss=0)
 # circuit = circuitTemplate([bounds[i][0] for i in circuitTemplate.parameters])
 # frequencies, frequencyResponse = circuit.getFrequencyResponse()
@@ -56,28 +66,30 @@ optimizer = sizer.optimizers.Optimizer(circuitTemplate, loss, bounds, earlyStopL
 circuit = optimizer.run()
 print(circuit.netlist)
 print("total loss:", loss(circuit))
-print("optimal parameters", dict(zip(circuitTemplate.parameters, circuit.parameters)))
+print("optimal parameters",
+      dict(zip(circuitTemplate.parameters, circuit.parameters)))
 print("bandwidth:", circuit.bandwidth)
 print("gain:", circuit.gain)
 print("phase margin:", circuit.phaseMargin)
-# exit()
 
-import matplotlib.pyplot as plt
 
-plt.rcParams["axes.grid"] = True
+if _PLOT:
+    plt.rcParams["axes.grid"] = True
 
-frequencies, frequencyResponse = circuit.getFrequencyResponse()
+    frequencies, frequencyResponse = circuit.getFrequencyResponse()
 
-plt.subplot(211)
-plt.plot(frequencies, np.abs(frequencyResponse))
-plt.xscale("log")
-plt.yscale("log")
-plt.vlines(sizer.calculators.unityGainFrequency(frequencies, frequencyResponse), 0, 1e+3)
+    plt.subplot(211)
+    plt.plot(frequencies, np.abs(frequencyResponse))
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.vlines(sizer.calculators.unityGainFrequency(
+        frequencies, frequencyResponse), 0, 1e+3)
 
-plt.subplot(212)
-phaseResponse = np.angle(frequencyResponse, deg=True)
-phaseResponse[np.where(phaseResponse > 0)] -= 360
-plt.plot(frequencies, phaseResponse)
-plt.xscale("log")
-plt.vlines(sizer.calculators.unityGainFrequency(frequencies, frequencyResponse), -180, 0)
-plt.show()
+    plt.subplot(212)
+    phaseResponse = np.angle(frequencyResponse, deg=True)
+    phaseResponse[np.where(phaseResponse > 0)] -= 360
+    plt.plot(frequencies, phaseResponse)
+    plt.xscale("log")
+    plt.vlines(sizer.calculators.unityGainFrequency(
+        frequencies, frequencyResponse), -180, 0)
+    plt.show()
