57cb6e8adac6a446b26612e82ad507bf74a08d80
Danilo Bizarria <unifdaoliveira@fei.edu.br>
2024-05-17 18:48:32 -0300

    add single_stage_amp module and use it in compare.py;
    use it to re-evaluate saved DEAP pop;
    and to simulate Leme data from scratch.

--- compare.py
+++ compare.py
@@ -4,7 +4,10 @@ import pickle
 
 from deap.tools._hypervolume import hv
 from math import log10
+from sklearn.metrics import r2_score
 from sklearn.preprocessing import MinMaxScaler
+
+from single_stage_amp import creator, evaluate
 
 
 # dB = 20 log_{10}(ratio)
@@ -20,7 +23,7 @@ def ratio_from_db(db):
 seed_leme = 1241
 timestamp = "2024-05-17_10-55"
 model = "deap_nsga3-params_deb"
-seed_deap = 1241
+seed_deap = seed_leme
 gen = None
 
 leme_data = pandas.read_csv("leme-fronteira.csv", delimiter="\t")
@@ -61,76 +64,142 @@ Y_train_weighted = Y_train * y_weights
 
 ref_leme = numpy.max(Y_train_weighted, axis=0) + 1
 
-x_scaler = MinMaxScaler()
-y_scaler = MinMaxScaler()
+leme_scaler = MinMaxScaler()
 
-X_train_scaled_self = pandas.DataFrame(
-    x_scaler.fit_transform(X_train), columns=X_train.columns)
-Y_train_scaled_self = pandas.DataFrame(
-    y_scaler.fit_transform(Y_train), columns=Y_train.columns)
+Y_train_scaled_leme = pandas.DataFrame(
+    leme_scaler.fit_transform(Y_train), columns=Y_train.columns)
 
-Y_train_scaled_self_weighted = Y_train_scaled_self * y_weights
+Y_train_scaled_leme_weighted = Y_train_scaled_leme * y_weights
 
-ref_leme_scaled = numpy.max(Y_train_scaled_self_weighted, axis=0) + 1
+ref_leme_scaled_leme = numpy.max(Y_train_scaled_leme_weighted, axis=0) + 1
 
 prefix = f"./out/single-stage-amp/{timestamp}_{model}-seed_{seed_deap}-"
 fname = prefix + "pop" + ("" if gen is None else f"-gen_{gen}") + ".pickle"
 with open(fname, "rb") as pop_file:
     pop = pickle.load(pop_file)
 
+# pickled pop already has fitness values, but evaluate() has
+# been updated.
+fitnesses = list(map(evaluate, pop))
+for ind, fit in zip(pop, fitnesses):
+    ind.fitness.values = fit
+
+X_pop = X_train.apply(creator.Individual, axis=1)
+Y_sim = list(map(evaluate, X_pop))
+
+for ind, fit in zip(X_pop, Y_sim):
+    ind.fitness.values = fit
+
+Y_sim = pandas.DataFrame(Y_sim, columns=Y_train.columns)
+
 obj = pandas.DataFrame(
-    [ind.fitness.values for ind in pop], columns=Y_train.columns)
+    fitnesses,
+    columns=Y_train.columns
+)
 wobj = pandas.DataFrame(
     numpy.array([ind.fitness.wvalues for ind in pop]) * -1,
-    columns=Y_train.columns)
+    columns=Y_train.columns
+)
+
+Y_sim_weighted = pandas.DataFrame(
+    numpy.array([ind.fitness.wvalues for ind in X_pop]) * -1,
+    columns=Y_train.columns
+)
+
+Y_sim_r2 = pandas.Series(
+    r2_score(Y_train, Y_sim, multioutput="raw_values"),
+    index=Y_train.columns
+)
 
 ref_deap = numpy.max(wobj, axis=0) + 1
+ref_leme_sim = numpy.max(Y_sim_weighted, axis=0) + 1
 
 obj_scaled_leme = pandas.DataFrame(
-    y_scaler.transform(obj), columns=Y_train.columns)
+    leme_scaler.transform(obj),
+    columns=Y_train.columns
+)
+
+Y_sim_scaled_leme = pandas.DataFrame(
+    leme_scaler.transform(Y_sim),
+    columns=Y_train.columns
+)
 
 wobj_scaled_leme = obj_scaled_leme * y_weights
 
-obj_scaler = MinMaxScaler()
+Y_sim_scaled_leme_weighted = Y_sim_scaled_leme * y_weights
 
-obj_scaled_self = pandas.DataFrame(
-    obj_scaler.fit_transform(obj), columns=Y_train.columns)
+deap_scaler = MinMaxScaler()
+
+obj_scaled_deap = pandas.DataFrame(
+    deap_scaler.fit_transform(obj),
+    columns=Y_train.columns
+)
 
 Y_train_scaled_deap = pandas.DataFrame(
-    obj_scaler.transform(Y_train), columns=Y_train.columns)
+    deap_scaler.transform(Y_train),
+    columns=Y_train.columns
+)
+
+Y_sim_scaled_deap = pandas.DataFrame(
+    deap_scaler.transform(Y_sim),
+    columns=Y_train.columns
+)
+
+wobj_scaled_deap = obj_scaled_deap * y_weights
 
 Y_train_scaled_deap_weighted = Y_train_scaled_deap * y_weights
 
-wobj_scaled_self = obj_scaled_self * y_weights
+Y_sim_scaled_deap_weighted = Y_sim_scaled_deap * y_weights
 
-ref_deap_scaled = numpy.max(wobj_scaled_self, axis=0) + 1
+ref_deap_scaled = numpy.max(wobj_scaled_deap, axis=0) + 1
 
 
 def main():
     print("deap:", timestamp, model, seed_deap, gen, "leme:", seed_leme)
 
     # TODO: reevaluate the relevance of raw hypervolume.
-    # print("raw hv:")
-    # print("leme:", hv.hypervolume(Y_train_weighted.to_numpy(), ref_leme.to_numpy()))
-    # print("deap:", hv.hypervolume(wobj.to_numpy(), ref_deap.to_numpy()))
+    print("raw hv:")
+    print("leme:", hv.hypervolume(Y_train_weighted.to_numpy(), ref_leme.to_numpy()))
+    print("deap:", hv.hypervolume(wobj.to_numpy(), ref_deap.to_numpy()))
+    print("simulated:", hv.hypervolume(
+        Y_sim_weighted.to_numpy(), ref_leme_sim.to_numpy()
+    ))
 
     print("\nleme-scaled hv:")
     print("leme:", hv.hypervolume(
-        Y_train_scaled_self_weighted.to_numpy(),
-        ref_leme_scaled.to_numpy()))
+        Y_train_scaled_leme_weighted.to_numpy(),
+        ref_leme_scaled_leme.to_numpy()
+    ))
     print("deap:", hv.hypervolume(
         wobj_scaled_leme.to_numpy(),
-        ref_leme_scaled.to_numpy()))
+        ref_leme_scaled_leme.to_numpy()
+    ))
+    print("simulated:", hv.hypervolume(
+        Y_sim_scaled_leme_weighted.to_numpy(),
+        ref_leme_scaled_leme.to_numpy()
+    ))
 
     # TODO: reevaluate the relevance of scaling the
     # hypervolume to the deap result.
-    # print("\ndeap-scaled hv:")
-    # print("leme:", hv.hypervolume(
-    #     Y_train_scaled_deap_weighted.to_numpy(),
-    #     ref_deap_scaled.to_numpy()))
-    # print("deap:", hv.hypervolume(
-    #     wobj_scaled_self.to_numpy(),
-    #     ref_deap_scaled.to_numpy()))
+    print("\ndeap-scaled hv:")
+    print("leme:", hv.hypervolume(
+        Y_train_scaled_deap_weighted.to_numpy(),
+        ref_deap_scaled.to_numpy()
+    ))
+    print("deap:", hv.hypervolume(
+        wobj_scaled_deap.to_numpy(),
+        ref_deap_scaled.to_numpy()
+    ))
+    print("simulated:", hv.hypervolume(
+        Y_sim_scaled_deap_weighted.to_numpy(),
+        ref_deap_scaled.to_numpy()
+    ))
+
+    if seed_deap == seed_leme:
+        print("simulation score:")
+        print("raw:")
+        print(Y_sim_r2)
+        print("avg:", numpy.mean(Y_sim_r2))
 
 
 if __name__ == "__main__":
---
+++ single_stage_amp.py
@@ -0,0 +1,117 @@
+import numpy
+
+from deap import base, creator
+from sizer import CircuitTemplate
+
+
+# ipol, vpol, l12, l34, l56, l78, l9, l10
+#             w12, w34, w56, w78, w9, w10
+IND_SIZE = 14
+
+# 0,1\micro\ampere, 100\micro\ampere
+I_MIN, I_MAX = 0.1e-6, 100e-6
+
+# 0,5\micro\meter, 100\micro\meter
+LW_MIN, LW_MAX = 0.5e-6, 100e-6
+
+# 0,7\volt, 2,3\volt
+V_MIN, V_MAX = 0.7, 2.3
+
+IV = ["ipol", "vpol"]
+PAIRS = ["12", "34", "56", "78", "9", "10"]
+LS = ["l" + p for p in PAIRS]
+WS = ["w" + p for p in PAIRS]
+
+BOUND_LOW = [I_MIN, V_MIN] + [LW_MIN] * (len(LS) + len(WS))
+BOUND_UP = [I_MAX, V_MAX] + [LW_MAX] * (len(LS) + len(WS))
+
+NDIM = 2 + len(LS) + len(WS)
+
+# Multiobjective
+# A_{v0}, f_T, Pwr, SR, Area
+NOBJ = 5
+
+
+# NOTE: area statistic.
+def area_key(individual):
+    a = 0.0
+
+    ls = individual[2:(2 + len(LS))]
+    ws = individual[-len(WS):]
+    assert len(ls) == len(ws)
+
+    for l, w in zip(ls, ws):
+        a += 2 * l * w
+
+    # l_pairs = ls[:-2]
+    l_singles = ls[-2:]
+
+    # w_pairs = ws[:-2]
+    w_singles = ws[-2:]
+
+    # reiterate over singles, as there are only two of them.
+    for l, w in zip(l_singles, w_singles):
+        a -= l * w
+
+    return a
+
+
+def evaluate(individual):
+    params = params_from_ind(individual)
+    circuit = circuitTemplate(params)
+
+    # TODO: look for a more elegant way than one try-except
+    # for each circuit metric.
+    # NOTE: Don't use a single try-except, as a good
+    # performance in any single variable is worth keeping.
+    try:
+        gain = numpy.absolute(circuit.gain)
+    except:
+        gain = 0
+
+    try:
+        # bandwidth = circuit.bandwidth
+        bandwidth = circuit.unityGainFrequency
+    except:
+        bandwidth = 0
+
+    try:
+        power = circuit.staticPower
+    except:
+        power = numpy.inf
+
+    try:
+        # TODO: evaluate the need for hints.
+        slew_rate = circuit.slewRate
+    except:
+        slew_rate = 0
+
+    # NOTE: area doesn't throw exceptions.
+    area = area_key(individual)
+
+    return gain, bandwidth, power, slew_rate, area
+
+
+def params_from_ind(individual):
+    return dict(zip((IV + LS + WS), individual))
+
+
+# Clean up to run script interactively (ipython).
+classes = [
+    "FitnessAmp",
+    # "FitnessMax",
+    # "FitnessMin",
+    "Individual",
+]
+for c in classes:
+    if hasattr(creator, c):
+        delattr(creator, c)
+
+with open("./demos/single-stage-amplifier/single-stage-amp.cir") as f:
+    circuitTemplate = CircuitTemplate(f.read())
+
+# A_{v0}, f_T, Pwr, SR, Area
+creator.create("FitnessAmp", base.Fitness, weights=(1.0, 1.0, -1.0, 1.0, -1.0))
+
+# Five amplifier metrics
+creator.create("Individual", list, fitness=creator.FitnessAmp)
